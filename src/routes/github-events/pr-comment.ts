7ae66002ffb273aaacf108ad5d62a596b135de8c
Roni Frantchi <roni-frantchi@users.noreply.github.com>
2021-12-23 15:43:39 +0200

    slack name

--- src/routes/github-events/pr-comment.ts
+++ src/routes/github-events/pr-comment.ts
@@ -2,11 +2,13 @@ import * as aws from '@pulumi/aws';
 import { byGitHubDiscussion, slackThreadGithubDiscussionTable } from '../../tables/slack-thread-github-discussion';
 import { pullRequestToChannelName } from '../../commons/github/pull-request-to-channel-name';
 import axios from 'axios';
+import { getSlackUser } from '../../commons/user-map/get-slack-user'
 
 export const prComment = async (slackbotAuthToken: string, body: any) => {
   // Create top-level comment
   const slackChannel = pullRequestToChannelName(body.pull_request);
-  const response = await axios.post('https://slack.com/api/chat.postMessage', { token: slackbotAuthToken, channel: slackChannel, text: body.comment.body});
+  const slackName = getSlackUser(body.sender.login);
+  const response = await axios.post('https://slack.com/api/chat.postMessage', { token: slackbotAuthToken, channel: slackChannel, text: body.comment.body, username: slackName });
   const slackThreadId = response.data.ts;
 
   // Store comment in Dynamo
