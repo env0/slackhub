18e3e348fa83011b4ec14c902847b1ab941d1f45
Roni Frantchi <roni.frantchi@gmail.com>
2021-12-23 14:34:11 +0200

    map comment to thread

--- src/tables/slack-thread-github-discussion.ts
+++ src/tables/slack-thread-github-discussion.ts
@@ -1,17 +1,17 @@
 import * as aws from '@pulumi/aws';
 
 export const bySlackThread = 'slackThreadIndex';
-export const byGitHubDiscussion = 'githubDiscussionIndex';
+export const byGitHubComment = 'githubCommentIndex';
 export const slackThreadGithubDiscussionTable = new aws.dynamodb.Table('slackThreadGithubDiscussion', {
   attributes: [
     { name: 'id', type: 'S' },
     { name: 'slackThreadId', type: 'S' },
-    { name: 'githubDiscussionId', type: 'S' }
+    { name: 'githubCommentId', type: 'S' }
   ],
   hashKey: 'id',
   billingMode: 'PAY_PER_REQUEST',
   globalSecondaryIndexes: [
     { name: bySlackThread, hashKey: 'slackThreadId', projectionType: 'ALL' },
-    { name: byGitHubDiscussion, hashKey: 'githubDiscussionId', projectionType: 'ALL' }
+    { name: byGitHubComment, hashKey: 'githubCommentId', projectionType: 'ALL' }
   ]
 });
